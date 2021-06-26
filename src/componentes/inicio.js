5321796ae2746cdc747ec7396dfcebd6e36a3cc7
viniciusmoraes07 <viniciusengcefet@hotmail.com>
2021-06-26 20:27:48 -0300

    branchteste dividida

--- src/componentes/inicio.js
+++ src/componentes/inicio.js
@@ -62,7 +62,7 @@ export default class Principal extends Component{
 
                    <a href="#" style={{color:'white',marginLeft:30,marginTop:90,textDecoration:'none',verticalAlign:'middle'}}>Estatística</a>
 
-                   <a href="#s" style={{color:'white',marginLeft:30,marginTop:90,textDecoration:'none',verticalAlign:'middle'}}>Saiba Mais</a>
+                   <a href="#ss" style={{color:'white',marginLeft:30,marginTop:90,textDecoration:'none',verticalAlign:'middle'}}>Saiba Mais</a>
                      
                    
                 </div>
